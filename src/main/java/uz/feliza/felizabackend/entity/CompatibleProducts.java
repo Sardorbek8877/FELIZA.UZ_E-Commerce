82a929d058d5171dc0c6f52086a682539a5d1ff1
Sardorbek8877 <sardorbeksafarov8844@gmail.com>
2023-11-01 10:52:22 +0100

    error remove

---
+++ src/main/java/uz/feliza/felizabackend/entity/CompatibleProducts.java
@@ -0,0 +1,20 @@
+package uz.feliza.felizabackend.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import uz.feliza.felizabackend.entity.template.AbstractLongEntity;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CompatibleProducts extends AbstractLongEntity {
+
+    private List<Long> compatibleProductsIdList;
+}
